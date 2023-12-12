528edfcda8a0c1c52cd4c18c1f2d01004b085e60
rezha4 <rezha.bahari@gmail.com>
2023-12-12 09:54:48 +0700

    cors adding origin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 const userController = require("./controllers/userController");
